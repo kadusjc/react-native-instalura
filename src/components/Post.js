e552009a0cd1ecffbd8b77ad908a2979ea6a17d7
Eng. Carlos E F Correa <kadusjc1984@gmail.com>
2020-01-24 08:41:35 -0300

    Finished Part 1

---
+++ src/components/Post.js
@@ -0,0 +1,109 @@
+import React, { Component } from 'react'
+import { Text, View, Image, TouchableOpacity, TextInput } from 'react-native'
+import PostStyles from '../styles/post-styles'
+import { isEmpty, get } from 'lodash'
+
+const s2Empty = require('../../resources/images/s2Empty.png')
+const s2Filled = require('../../resources/images/s2Filled.png')
+const sendImg = require('../../resources/images/send.jpeg')
+const login = 'kadusjc1984'
+
+export default class Post extends Component {  
+
+  constructor (props) {
+    super(props)
+    this.state = { photo: this.props.photo, commentValue: '' }
+  }  
+
+  loadS2Icon (photo) {
+    return photo.likeada ?  s2Filled: s2Empty
+  }
+
+  drawSubtitle (photo) {
+    return (
+      <View style={PostStyles.subtitle} key='legendaFoto'>
+        <Text style={PostStyles.titleComments}>{photo.loginUsuario}</Text>
+        <Text>{photo.comentario}</Text>
+      </View>
+    )
+  }  
+
+  drawComments (photo) {
+    const comentarios = photo.comentarios
+    return comentarios.map((comm) => (
+      <View style={PostStyles.comments} key={comm.id}>
+        <Text id={comm.id} style={PostStyles.titleComments}>{comm.login}</Text>
+        <Text id={comm.id}>{comm.texto}</Text>
+      </View>
+    ))
+  }
+
+  drawLikes (likers = []) {
+    if (!likers.length) return
+
+    let toPrint = `${likers.length} curtida`
+    if (likers.length > 1) toPrint += 's'
+    return (
+      <Text style={PostStyles.likes}>
+        {toPrint}
+      </Text>
+    )
+  }
+
+  onPhotoLike (photo) {
+    let likers = []    
+    if (!photo.likeada) {
+      likers = photo.likers.concat({login})
+    } else {
+      likers = photo.likers.filter(liker => liker.login !== login)
+    }
+    const updatedPhoto = {...photo, likeada: !photo.likeada, likers}
+    this.setState({photo: updatedPhoto})
+  }
+
+  onAddComment () {
+    if (isEmpty(get(this.state, 'commentValue'))) return
+
+    const commentsRepository = [...this.state.photo.comentarios]
+    commentsRepository.push({id: this.state.commentValue, login, texto: this.state.commentValue})   
+    const photo = {...this.state.photo, comentarios: commentsRepository}
+    this.setState({photo, commentValue: ''})
+    this.newCommentInput.clear()
+  }
+
+  render () {
+    const { photo } = this.state
+    return (
+        <View key='main'>
+            <View style={PostStyles.header} key='header'>
+                <Image source={{uri: photo.urlPerfil}} style={PostStyles.profilePhoto} />
+                <Text>{photo.loginUsuario}</Text>  
+            </View>
+            <TouchableOpacity onPress={() => this.onPhotoLike(photo)}>
+              <Image source={{uri: photo.urlFoto}} style={PostStyles.postPhoto} />        
+            </TouchableOpacity>
+            <View style={PostStyles.footer}>
+                <TouchableOpacity onPress={() => this.onPhotoLike(photo)}>
+                    <Image source={this.loadS2Icon(photo)} style={PostStyles.likeButton}/>        
+                </TouchableOpacity>
+                {this.drawLikes(photo.likers)}
+                {this.drawSubtitle(photo)}
+                {this.drawComments(photo)}
+                
+                <View style={PostStyles.newComments} key='newComent'>
+                  <TextInput style={PostStyles.input}
+                    placeholder='Adicione um comentário...'
+                    ref={input => this.newCommentInput = input}
+                    onChangeText={text => this.setState({commentValue: text})}
+                    onSubmitEditing={this.onAddComment.bind(this)}/>
+
+                  <TouchableOpacity onPress={this.onAddComment.bind(this)}>
+                    <Image style={PostStyles.sendButton} source={sendImg}/>  
+                  </TouchableOpacity>  
+
+                </View>
+            </View>
+        </View>
+    )
+  }
+}
---
+++ src/styles/post-styles.js
@@ -0,0 +1,58 @@
+import {StyleSheet, Dimensions} from 'react-native'
+const width = Dimensions.get('screen').width
+
+module.exports = StyleSheet.create({ 
+  header: {
+    flexDirection: 'row', 
+    alignItems: 'center', 
+    margin: 10
+  },
+  profilePhoto: {
+    width: 40, 
+    borderRadius: 20, 
+    height: 40, 
+    marginRight: 10
+  },
+  postPhoto: {
+    width, 
+    height: width
+  },
+  likeButton: {
+    height: 30,
+    width: 30,
+    marginBottom: 10
+  },
+  footer: {
+    margin: 10
+  },
+  subtitle: {
+    flexDirection: 'row', 
+    marginBottom: 10
+  },
+  comments: {
+    flexDirection: 'row', 
+  }, 
+  newComments: {
+    borderBottomColor: '#ddd',
+    borderBottomWidth: 1,
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  likes: {
+    fontWeight: 'bold'
+  },
+  titleComments: {
+    fontWeight: 'bold',
+    marginRight: 5
+  },
+  input: {
+    height: 40,
+    fontSize: 17,
+    flex: 1
+  },
+  sendButton: {
+    width: 35,
+    height: 35
+  }
+})
+
